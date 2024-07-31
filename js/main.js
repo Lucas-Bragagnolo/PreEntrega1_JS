20316d4d5a6ca0b83f01d0ca2381645c108750bd
Lucas Bragagnolo <lucasbragagnolo6@gmail.com>
2024-07-30 23:31:24 -0300

    V1.2 Funciones hechas (Listo para entregar pero puede ser optimizado)

--- js/main.js
+++ js/main.js
@@ -12,7 +12,10 @@ function mostrar(valor) {
 
 let pinGuardado = "2802";
 let ingresar=false;
-
+const TARIFA = 380;
+let tarifa1 = TARIFA;
+let tarifa2 = TARIFA*2;
+let tarifa3 = TARIFA*3;
 
 for(let i = 3; i >= 1; i = i - 1) {
     let pinIngreso = prompt("Ingresá el PIN"); 
@@ -32,17 +35,24 @@ for(let i = 3; i >= 1; i = i - 1) {
 //Segunda pantalla
 if(ingresar) {
     //menu 
-    let saldo = 50000;
+    let saldo = 1000;
+
 
     function recargar(valor) {
         saldo= saldo + valor;
     }
-    
     function pagar(valor) {
-        saldo = saldo - valor;
+        if(saldo<valor) {
+            alert("Saldo insuficiente");
+        } else {
+            saldo = saldo - valor;
+            alert("Viaje exitoso! Pagaste: $" + valor +".00\nAhora tu saldo es de: " + saldo + "\nMuchas Gracias!");
+        }        
     }
-    
 
+
+    
+    
     let opcion = prompt (
         "Elegí una opcion: \n1- Consultar Saldo \n2- Iniciar Viaje \n3- Recargar saldo. \nPresioná X para salir"
     );
@@ -52,38 +62,63 @@ if(ingresar) {
             alert("Tu saldo es " + saldo);
 
         } else if(opcion=="2") {
-
-            let puntoPartida = prompt ("Ingresá el punto de partida: \n1-Constitución \n2-Flores \n3-Liniers");
-
-
-
-
-            let puntoDestino = prompt ("Ingresá el punto de llegada: \n1-Constitución \n2-Flores \n3-Liniers");
-
-            let validarViaje = puntoPartida != puntoDestino;
-
-            if(validarViaje) {
-                let montoViaje = prompt ("Ingresá el monto a pagar:");
-
-                if(montoViaje > saldo){
-                    alert("Saldo insuficiente");
-                    opcion = prompt (
-                        "Elegí una opcion: \n1- Consultar Saldo \n2- Iniciar Viaje \n3- Recargar saldo. \nPresioná X para salir"
-                    );
-                } else {
-                    pagar(montoViaje);
-                    alert("Viaje exitoso! Tu saldo es: " + saldo + "Muchas Gracias!");
-                }
-            } else {
-                alert("Error! No se puede viajar al mismo punto");
+            let puntoDestino;            
+            let puntoPartida = prompt ("Donde empieza el viaje?: \n1-Constitución \n2-Flores \n3-Liniers");
+            switch(puntoPartida) {
+                case "1":
+                    puntoDestino = prompt ("A dónde vas?: \n1- Hasta Corrientes y callao (" + tarifa1 + ") \n2-Hasta Alto Palermo (" + tarifa2 + ") \n3-Hasta Barrio Chino (" + tarifa3 + ")\nSaldo: $" + saldo);
+                    switch(puntoDestino) {
+                        case "1":
+                                pagar(tarifa1);                    
+                            break;
+                        case "2":
+                                pagar(tarifa2);
+                            break;
+                        case "3":
+                                pagar(tarifa3);
+                            break;
+                        default:
+                            alert("Punto de destino incorrecto");
+                    }
+                    break;
+                case "2":
+                    puntoDestino = prompt ("A dónde vas?: \n1-Av.Avellaneda y la Av. plata ($" + tarifa1 + ") \n2-Plaza Almagro ($" + tarifa2 + ")\n3-Plaza de mayo ($" + tarifa3 + ") \nSaldo: $" + saldo);
+                    switch(puntoDestino) {
+                        case "1":
+                                pagar(tarifa1);                    
+                            break;
+                        case "2":
+                                pagar(tarifa2);
+                            break;
+                        case "3":
+                                pagar(tarifa3);
+                            break;
+                        default:
+                            alert("Punto de destino incorrecto");
+                    }
+                    break;
+                case "3":
+                    puntoDestino = prompt ("A dónde vas?: \n1-V.del Parque ($" + tarifa1 + ") \n2-V.Urquiza ($" + tarifa2 + ")\n3-Colegiales ($" + tarifa3 + ")\nSaldo: $" + saldo);
+                    switch(puntoDestino) {
+                        case "1":
+                                pagar(tarifa1);                    
+                            break;
+                        case "2":
+                                pagar(tarifa2);
+                            break;
+                        case "3":
+                                pagar(tarifa3);
+                            break;
+                        default:
+                            alert("Punto de destino incorrecto");
+                    }
+                    break;
+                default:
+                    alert("Punto de partida incorrecto");
             }
-
-
-
-
-
-
-
+            mostrar(puntoDestino);
+            mostrar(saldo);
+        
         } else if(opcion=="3") {
                 let deposito = prompt ("Ingresá el monto a recargar:");
                 saldo = saldo + +deposito;
